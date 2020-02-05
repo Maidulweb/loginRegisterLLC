9191a46762198695c5613d1fe4c0ed4d1c9fd04c
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-02-05 20:27:07 +0600

    Create new repository

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<div class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4"><p>This sample title</p></div>
+      <div class="col-md-4 text-center"><p>Here is social icon</p></div>
+      <div class="col-md-4 text-right"><p><a href="">Login</a>  <a href="">Register</a></p></div>
+    </div>
+  </div>
+</div>
