3641d1d9cbaf3c56c7b8fd147a44526cfdeee529
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-02-06 08:37:09 +0600

    create model

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
+Route::get('/', 'indexController@index')->name('index');
